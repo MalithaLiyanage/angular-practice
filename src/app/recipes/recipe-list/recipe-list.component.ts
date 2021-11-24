423e18ec932c6d8033beedf9eb5d5a1e4d310431
Malitha Liyanage <malitharanu@gmail.com>
2021-11-24 16:22:04 +0530

    displayed recipes in a list]

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Vegetable Chopseuy", "Bunch of vegetables get cut and cooked with cornflour", "https://sp-ao.shortpixel.ai/client/to_auto,q_lossy,ret_img,w_1200/https://norecipes.com/wp-content/uploads/2017/04/chop-suey-010-1200x800.jpg"),
+    new Recipe("Vegetable Chopseuy", "Bunch of vegetables get cut and cooked with cornflour", "https://sp-ao.shortpixel.ai/client/to_auto,q_lossy,ret_img,w_1200/https://norecipes.com/wp-content/uploads/2017/04/chop-suey-010-1200x800.jpg")
+  ];
 
   constructor() { }
 
